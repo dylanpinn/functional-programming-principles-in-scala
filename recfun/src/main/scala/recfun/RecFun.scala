58d39e0860c36a56ca752bd6f3d7f44f58e20873
Dylan Pinn <dylan.pinn@rea-group.com>
2021-06-04 09:37:30 +1000

    First attempt at pascal exercise

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -12,7 +12,11 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c <= 0 || c == r)
+      return 1
+    pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
